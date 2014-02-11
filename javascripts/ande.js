e5ddcacd05f58804906252c914c49ad7f12cb80e
Schlechtwetterfront <schlchtwtrfrnt@gmail.com>
2014-02-11 12:26:16 +1000

    Added nice project view.

--- javascripts/ande.js
+++ javascripts/ande.js
@@ -4,3 +4,31 @@ function toggleHide(divID) {
         item.className = (item.className == 'hidden') ? 'unhidden' : 'hidden';
     }
 }
+
+
+var projects = {
+    'zet': {'header': 'XSIZETools', 'info': 'ZeroEngine .msh exporter and importer for Softimage'},
+    'softcry': {'header': 'SoftCry', 'info': 'CRYENGINE exporter for Softimage'},
+    'modosrcr': {'header': 'modosourcerer', 'info': 'Source Engine exporter for modo'},
+    'zefrmt': {'header': 'ZeroEngine Docs', 'info': 'Information about ZeroEngine file formats'}
+};
+
+
+function setProjectInfo(project) {
+    var header = document.getElementById('project_header');
+    var body = document.getElementById('project_body');
+    var project = projects[project];
+    if (header) {
+        header.innerHTML = project['header'];
+    }
+    if (body) {
+        body.innerHTML = project['info'];
+    }
+}
+
+
+function clearProjectInfo() {
+    var header = document.getElementById('project_header');
+    var body = document.getElementById('project_body');
+    header.innerHTML = body.innerHTML = '';
+}
