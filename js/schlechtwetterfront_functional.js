755911187c699463d577cd8c2bc8d6d2202aa544
Benedikt Schatz <schlechtwetterfront@gmail.com>
2015-09-26 16:17:12 +0200

    Script and minor style changes.

---
+++ js/schlechtwetterfront_functional.js
@@ -0,0 +1,109 @@
+function slideToggleContent(id) {
+    if (id.substr(0, 1) != "#") {
+        id = "#" + id;
+    }
+    var content = id + "_content";
+
+    $(content).slideToggle(250, function () {
+        $(content + "_start").text(function () {
+            return $(content).is(":visible") ? "Collapse" : "Expand";
+        })
+    });
+}
+
+function showContent(id) {
+    if (id.substr(0, 1) != "#") {
+        id = "#" + id;
+    }
+    var content = id + "_content";
+    if ($(content).is(":visible")) {
+        return;
+    }
+    $(content).slideToggle(250, function () {
+        $(content + "_start").text(function () {
+            return $(content).is(":visible") ? "Collapse" : "Expand";
+        })
+    });
+}
+
+
+window.addEventListener("load", function() {
+
+    $("a.toggle_collapse").click(function () {
+        var header = $(this);
+        var content = $("#" + $(this).attr("content-id"));
+
+        $(content).slideToggle(250, function () {
+
+            header.text(function () {
+
+                return $(content).is(":visible") ? "Collapse" : "Expand";
+            });
+        });
+
+    });
+
+    $("a.toggle_collapse_end").click(function () {
+        var header = $(this);
+        var content_text = "#" + $(this).attr("content-id");
+        var content = $("#" + $(this).attr("content-id"));
+
+        $(content).slideToggle(250, function () {
+            $(content_text + "_start").text(function () {
+                return $(content).is(":visible") ? "Collapse" : "Expand";
+            })
+        });
+
+    });
+
+    $("a.toggle_collapse").each(function () {
+        var header = $(this);
+        var content = $("#" + $(this).attr("content-id"));
+
+        if ($(this).attr("starts-collapsed") == "True") {
+            $(content).slideToggle(0, function () {
+
+                header.text(function () {
+
+                    return $(content).is(":visible") ? "Collapse" : "Expand";
+                });
+            });
+        }
+    });
+
+
+    $("a.rectangle_button").click(function() {
+        var id = $(this).attr("href");
+        showContent(id);
+    });
+
+
+    var indent_width = "2px";
+    indent_width = $("indent-child").css("width");
+
+    $("a.rectangle_button").hover(
+        function() {
+            $(this).children("indent-child").css({"visibility": "hidden"});
+            // $(this).children("indent-child").stop().animate({"width": 0}, {
+            //     duration: 300,
+            //     fail: function() { $(this).css("visibility", "visible"); }
+            // });
+        },
+        function() {
+            $(this).children("indent-child").css({"visibility": "visible", "width": indent_width});
+            // $(this).children("indent-child").stop().animate({"width": indent_width}, {
+            //     duration: 300,
+            //     always: function() { $(this).css("visibility", "visible"); }
+            // });
+        }
+    );
+});
+
+
+$(document).ready(function () {
+    var parts = window.location.href.split("#");
+    if(parts.length > 1) {
+       var id = parts[ parts.length-1 ];
+       showContent(id);
+    }
+});
---
+++ js/schlechtwetterfront_visual.js
@@ -0,0 +1,106 @@
+function shadeBlend(p,c0,c1) {
+    var n=p<0?p*-1:p,u=Math.round,w=parseInt;
+    if(c0.length>7){
+        var f=c0.split(","),t=(c1?c1:p<0?"rgb(0,0,0)":"rgb(255,255,255)").split(","),R=w(f[0].slice(4)),G=w(f[1]),B=w(f[2]);
+        return "rgb("+(u((w(t[0].slice(4))-R)*n)+R)+","+(u((w(t[1])-G)*n)+G)+","+(u((w(t[2])-B)*n)+B)+")"
+    }else{
+        var f=w(c0.slice(1),16),t=w((c1?c1:p<0?"#000000":"#FFFFFF").slice(1),16),R1=f>>16,G1=f>>8&0x00FF,B1=f&0x0000FF;
+        return "#"+(0x1000000+(u(((t>>16)-R1)*n)+R1)*0x10000+(u(((t>>8&0x00FF)-G1)*n)+G1)*0x100+(u(((t&0x0000FF)-B1)*n)+B1)).toString(16).slice(1)
+    }
+}
+
+var colors = [
+    "#ff7676", // reddish
+    // "#8d76ff", // darker violet
+    // "#d77f64", // orange
+    "#919ddb", // brighter blue
+    "#6AAF6A", // green
+];
+
+
+window.addEventListener("load", function() {
+
+    // Choose random hover color for all elments.
+    var box_hover_color = colors[Math.floor(Math.random() * colors.length)];
+
+    var box_darkened_color = shadeBlend(-0.3, box_hover_color);
+
+    var box_brightened_color = shadeBlend(0.5, box_hover_color);
+
+    var box_normal_color = "#252525";
+
+    // I LOVE HACKS FeelsGoodMan
+    var theme_map = {
+        "rgb(255, 255, 255)": "bright",
+        "rgb(0, 0, 0)": "dark",
+    };
+
+    var bg_color = $("head").css("background-color");
+    var theme = theme_map[bg_color];
+
+    var link_color_map_hover = {
+        "bright": box_darkened_color,
+        "dark": box_brightened_color,
+    };
+
+
+    // General link hover color and style change.
+
+    $("div.content a").css({"color": box_hover_color, "border-bottom": "1px dotted " + box_hover_color});
+    $("div.content a").hover(
+    	function() {
+    		$(this).css({"color": link_color_map_hover[theme],
+    					"border-bottom": "1px solid " + link_color_map_hover[theme]});
+    	},
+    	function() {
+    		$(this).css({"color": box_hover_color,
+    					"border-bottom": "1px dotted " + box_hover_color,
+    					"text-decoration": "none"});
+    	}
+    );
+
+
+    // Main page hover color change.
+
+    var info_margin = 4;
+    info_margin = parseInt($(".box").attr("margin"));
+    $("info").css({"padding-top": info_margin, "padding-bottom": info_margin});
+
+    if ($(window).width() <= 1020) {
+        $(".info").css("background-color", box_hover_color);
+        return;
+    }
+
+    // Set the height here for the slide functionality.
+    // If js is disabled then the box will just be twice the height so all of the content is visible.
+    $(".box").css("height", "128");
+    // Expanding boxes and random hover color for project boxes.
+    $(".box").hover(
+        function () {
+            $(this).css("background-color", box_hover_color);
+
+
+            var id = $(this).attr("id");
+
+            $("#" + id + "_title").css("top", "-128px");
+            $("#" + id + "_content").css("top", "-128px");
+        },
+        function () {
+            // $(this).css("background-color", box_normal_color);
+            $(this).attr("style", "");
+            $(this).css("height", "128px");
+
+            var id = $(this).attr("id");
+            $("#" + id + "_title").css("top", "0px");
+            $("#" + id + "_content").css("top", "0px");
+
+        }
+    );
+
+
+    // Set project page side bar color.
+
+    $("div .sidebar").css("background-color", box_hover_color);
+
+});
+
