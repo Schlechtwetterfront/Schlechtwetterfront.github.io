29d276a63f0bb7521d404a0354cb8a2c77ce447d
Benedikt Schatz <schlchtwtrfrnt@gmail.com>
2017-05-03 15:28:09 +0200

    Add chunk hierarchy

---
+++ src/build_msh_hierarchy.py
@@ -0,0 +1,43 @@
+from collections import OrderedDict
+from json import load, dump
+
+
+def build_msh_hierarchy():
+    chunks = None
+    with open('mshformat.json') as stream:
+        chunks = load(stream, object_pairs_hook=OrderedDict)
+
+    hier = OrderedDict()
+    for key, chunk in chunks.items():
+        if chunk['name'] == 'ExampleChunk' \
+           or chunk['category'] in set(['Other', 'Deprecated']):
+            continue
+        chunk['simple_children'] = OrderedDict()
+        chunk['container_children'] = OrderedDict()
+        chunk['all_children'] = OrderedDict()
+        chunk['has_containers'] = False
+        for chunk_key in chunk['children']:
+            if chunks.get(chunk_key) and chunks[chunk_key]['children']:
+                chunk['container_children'][chunk_key] = chunks[chunk_key]
+                chunk['has_containers'] = True
+            elif chunks.get(chunk_key) and not chunks[chunk_key]['children']:
+                chunk['simple_children'][chunk_key] = chunks[chunk_key]
+
+            if chunks.get(chunk_key):
+                chunk['all_children'][chunk_key] = chunks[chunk_key]
+            else:
+                chunk['all_children'][chunk_key] = {
+                    'all_children': [],
+                    'name': chunk_key,
+                    'no_link': True,
+                }
+                print(chunk_key)
+        if not chunk['parent']:
+            hier[key] = chunk
+
+    with open('mshhierarchy.json', 'w') as stream:
+        dump(hier, stream)
+
+
+if __name__ == '__main__':
+    build_msh_hierarchy()
